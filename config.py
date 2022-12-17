95900898fb2974a20eeb8f9feb2e16f91e71857e
Kyosuke Tsuneda <k_tsuneda@yumemi.co.jp>
2022-12-17 17:10:12 +0900

    予約サイトにアクセスするための値を環境変数として用意

--- config.py
+++ config.py
@@ -4,3 +4,6 @@ load_dotenv()
 import os
 LINE_CHANNEL_ACCESS_TOKEN = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', '')
 LINE_USER_ID = os.getenv('LINE_USER_ID', '')
+WEBSITE_URL = os.getenv('WEBSITE_URL', '')
+WEBSITE_CARD_ID = os.getenv('WEBSITE_CARD_ID', '')
+WEBSITE_BIRTHDAY = os.getenv('WEBSITE_BIRTHDAY', '')
